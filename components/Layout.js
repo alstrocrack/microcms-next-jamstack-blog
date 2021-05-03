8e28d6898f303430d1bcda258f69459c4884227b
yuto urushima <yuto259233@gmail.com>
2021-05-03 22:52:02 +0900

    ogp & スクロール 修正

--- components/Layout.js
+++ components/Layout.js
@@ -7,14 +7,20 @@ export default function Layout(props) {
         <div className={layout.container}>
             <Head>
                 <title>YutoUrushima | {props.title}</title>
-                <meta property="og:url" content="https://pedantic-hugle-406857.netlify.app" />
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="YutoUrushima" />
-                <meta property="og:description" content="Next.js + microCMSで作ったデモブログサイトです。" />
-                <meta property="og:site_name" content="YutoUrushima" />
-                <meta property="og:image" content="./b-ogp-image.jpg" />
-                <meta name="twitter:card" content="Summary with Large Image" />
-                <meta name="twitter:site" content="@Frontend_1220" />
+                {/* 共通、facebook */}
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="YutoUrushima"/>
+                <meta property="og:description" content="Next.js + microCMSで作ったデモブログサイトです。"/>
+                <meta property="og:url" content="https://pedantic-hugle-406857.netlify.app"/>
+                <meta property="og:site_name" content="YutoUrushima"/>
+                {/* meta twitter */}
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:site" content="YutoUrushima"/>
+                <meta name="twitter:title" content="YutoUrushima"/>
+                <meta name="twitter:description" content="Next.js + microCMSで作ったデモブログサイトです。"/>
+                <meta name="twitter:image" content="https://pedantic-hugle-406857.netlify.app/b-ogp-image.jpg"/>
+                <meta name="twitter:url" content="https://pedantic-hugle-406857.netlify.app"/>
+                {/* /meta twitter */}
             </Head>
             <Header header={props.header} />
             {props.children}
